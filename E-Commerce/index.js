ab87c796da3b5829a6cf8dc2b1ae2a448abc5f4f
TosinJs <tosinjsamuel@gmail.com>
2021-12-22 22:13:07 +0100

    User Authentication and Authorization

--- E-Commerce/index.js
+++ E-Commerce/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("logger");
-const app = express();
 const connectDB = require("./db/index");
+const UserRouter = require("./routes/userRouter");
+const AuthRouter = require("./routes/authRouter");
 
-connectDB()
+connectDB();
+const app = express();
 
 //Using MiddleWare
-app.use(logger);
-app
+app.use(express.json({ extended: true }));
+app.use("/api/auth", AuthRouter);
+app.use("/api/user", UserRouter);
 
-
-
-
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on the Server")
 });
+
+app.use((error, request, response, next) => {
+    const { status = 500, message } = error;
+    console.log(status, message);
+    response.status(status).json({ status, message })
+})
